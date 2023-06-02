9ee9c368a52215517b883db54ee24d0a8708668f
Yi Lu <yi.lu@forethought.ai>
2023-06-02 15:59:22 -0700

    adding agent tests

---
+++ tests/agent/test_conversational_agent.py
@@ -0,0 +1,58 @@
+import json
+from typing import List, Optional
+
+from minichain.agent.conversational_agent import ConversationalAgent
+from minichain.agent.message import BaseMessage, AIMessage
+from minichain.agent.structs import AgentFinish
+from minichain.models.base import LLMResult, Generation, BaseLanguageModel
+from minichain.tools.simple_handoff.tools import HandOffToAgent
+
+
+class MockLLM(BaseLanguageModel):
+    message: str = ""
+
+    def generate(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+    ) -> LLMResult:
+        return LLMResult(generations=[Generation(message=AIMessage(content=self.message))])
+
+
+def test_should_answer_prompt():
+    agent = ConversationalAgent.from_llm_and_tools(
+        llm=MockLLM(message="yes, question is resolved"),
+        tools=[])
+
+    input = {"query": "user query", "history": "conversation history"}
+    response = agent.should_answer(**input)
+    assert isinstance(response, AgentFinish)
+
+    agent = ConversationalAgent(llm=MockLLM(message="no, question is not resolved"))
+    response = agent.should_answer(**input)
+    assert response is None
+
+
+def test_plan():
+    mock_generation_response = json.dumps({
+        "thoughts": {
+            "plan": "Given workflow policy and previous observations",
+            "need_use_tool": "Yes if needs to use another tool not used in previous observations else No"
+        },
+        "tool": {
+            "name": "",
+            "args": {
+                "arg_name": ""
+            }
+        },
+        "response": "response to suer",
+        "workflow_finished": "No"
+    })
+
+    agent = ConversationalAgent.from_llm_and_tools(
+        llm=MockLLM(message=mock_generation_response),
+        tools=[HandOffToAgent()])
+
+    input = {"query": "user query", "history": "conversation history"}
+    action = agent.plan(intermediate_steps=[], **input)
+    assert isinstance(action, AgentFinish)
