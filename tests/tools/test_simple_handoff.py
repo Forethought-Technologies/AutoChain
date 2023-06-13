93a9346f3fd373fc7d8f13eb5e236c4cc830a198
Yi Lu <yilu331@gmail.com>
2023-06-12 22:49:18 -0700

    cleanup handoff

--- tests/tools/test_simple_handoff.py
+++ tests/tools/test_simple_handoff.py
@@ -3,5 +3,5 @@ from minichain.tools.simple_handoff.tools import HandOffToAgent
 
 def test_simple_handoff() -> None:
     handoff = HandOffToAgent()
-    msg = handoff.run("")
-    assert "hand you off" in msg
+    msg = handoff.run()
+    assert handoff.handoff_msg == msg
